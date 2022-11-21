b4d8c6a95e998baef9e40de31cc95adf2478939f
jhonaquejadau <jhona.quejada@gmail.com>
2022-11-21 18:11:24 -0500

    gh-pages fix

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import {BrowserRouter} from "react-router-dom"
+import { ContextProvider } from './context/Context';
 import reportWebVitals from './reportWebVitals';
 
-import { ContextProvider } from './context/Context';
+import App from './App';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <ContextProvider>
-      <App />
-    </ContextProvider>
+    <BrowserRouter basename='/movies-app'>
+      <ContextProvider>
+        <App />
+      </ContextProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
