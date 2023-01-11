6a9e526f02b86772ef74b75945281b46a413c7a7
jhonaquejadau <jhona.quejada@gmail.com>
2023-01-10 19:50:41 -0500

    Header & Footer refactor

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { data } from "../utils/data";
+import tmdblogo from "../utils/logo/tmdblogo.svg";
 
 const Footer = () => {
   return (
-    <footer className=" w-full flex flex-row justify-center items-center py-[2em] px-[15em] bg-[#030337] max-[1000px]:px-[2em] max-[500px]:flex-col ">
-      <p className=" mr-auto max-[500px]:mr-0 max-[500px]:mb-4`} text-white">
-        {`<Design/>`} by{" "}
-        <span className="text-[#6cccff] font-[700]">jaqudev_ 2022</span>
+    <footer className="w-full flex flex-col justify-evenly items-center py-[2em] bg-[#030337] sm:flex-row">
+      <p className="max-[500px]:mb-4 text-white">
+        {`<Design/>`} by
+        <span className="text-[#6cccff] font-[700]"> jaqudev_ 2022</span>
       </p>
+      <img className="w-40 my-8 sm:my-0" src={tmdblogo} alt="tmdblogo" />
       <div className="flex flex-row justify-center items-center gap-4">
         {data.social.map((social, index) => {
           return (
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -27,27 +27,27 @@ export default function Header () {
     }, [])
 
     return (
-        <header className={`${changeNabvar ? 'bg-[rgba(0,0,0,0.8)]' : 'bg-gradient-to-b from-[rgba(0,0,0,0.5)] to-[rgba(0,0,0,0)]'} fixed z-10 top-0 left-0 w-[100%] text-slate-300 text-[1.2rem] py-2 `}>
+        <header className={`${changeNabvar ? 'bg-[rgba(0,0,0,0.9)]' : 'bg-gradient-to-b from-[rgba(0,0,0,0.5)] to-[rgba(0,0,0,0)]'} fixed z-10 top-0 left-0 w-[100%] text-slate-300 text-[1.2rem] py-2 `}>
             <div className="flex flex-row items-center w-[90%] h-full mx-auto">
                 <div onClick={() => handleShowNav()} className="hidden max-[750px]:block absolute z-10">
                     {!showNav ? <AiOutlineMenu /> : <AiOutlineClose/>}
                 </div>
-                <ul className={`${showNav ? 'block inset-0 bg-orange-500 absolute' : 'hidden'} flex flex-row justify-evenly items-center min-[750px]:flex`}>
+                <ul className={`${showNav ? 'block inset-0 bg-orange-500 absolute' : 'hidden'} flex flex-row justify-evenly items-center text-base min-[750px]:flex`}>
                     <Link to="/movies">
-                        <li onClick={() => handleHiddeNav()} className=" font-bold mx-2">Movies</li>
+                        <li onClick={() => handleHiddeNav()} className="font-[700] mx-2 hover:text-blue-900">Movies</li>
                     </Link>
                     <Link to="/tv-shows">
-                        <li onClick={() => handleHiddeNav()} className="font-bold mx-2">TV Shows</li>
+                        <li onClick={() => handleHiddeNav()} className="font-[700] mx-2 hover:text-blue-900">TV Shows</li>
                     </Link>
                 </ul>
                 <div className="mx-auto">
                     <Link to="/">
-                        <p className="text-[2rem] text-orange-500 font-bold mx-auto max-[750px]:text-[1.5rem]"><span className="hover:text-slate-100">MOVIE</span>  <span className="text-slate-400 hover:text-orange-300">BLOG</span></p>
+                        <p className="text-2xl text-orange-500 font-bold mx-auto max-[750px]:text-[1.5rem]"><span className="hover:text-slate-100">MOVIE</span>  <span className="text-slate-400 hover:text-orange-300">BLOG</span></p>
                     </Link>
                 </div>
                 <div className="flex flex-row justify-center items-center max-[750px]:hidden">
-                    <p className="capitalize font-bold mx-4 hover:text-rose-500">login</p>
-                    <p className="capitalize font-bold mx-4 hover:text-rose-500">TMDB</p>
+                    {/* <p className="capitalize font-bold mx-4 hover:text-rose-500">login</p> */}
+                    <p className="capitalize font-[800] text-base mx-4 hover:text-blue-900"><a href="https://developers.themoviedb.org/3/getting-started/introduction" target="_blank" rel="noreferrer">TMDB Documentation</a></p>
                 </div>
                 <div className="hidden max-[750px]:block text-2xl">
                     <AiOutlineLogin />
