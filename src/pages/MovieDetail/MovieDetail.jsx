45cc491c26450942668e822c8c2652c74d9c2bdd
jhonaquejadau <jhona.quejada@gmail.com>
2023-01-10 21:47:41 -0500

    MovieDetail - code refactor

--- src/pages/MovieDetail/MovieDetail.jsx
+++ src/pages/MovieDetail/MovieDetail.jsx
@@ -5,7 +5,6 @@ import { apiConfig } from "../../api/apiConfig";
 import { MoviesSlider } from "../../components";
 import { CardSlider, MoviesBanner, MovieVideos } from "../components";
 
-import "react-circular-progressbar/dist/styles.css";
 
 export const MovieDetail = () => {
   const { id } = useParams();
@@ -28,29 +27,23 @@ export const MovieDetail = () => {
   }, [id]);
 
   return (
-    <section className="flex flex-col w-[100%] h-full text-slate-200">
-      <MoviesBanner movie={movie} imagePath={apiConfig} />
-      <div className="bg-gradient-to-b from-black to-[#030337]">
-        <div className="flex flex-col justify-start w-[90%] mx-auto mt-6 max-[900px]:w-[100%] max-[900px]:p-4">
-          <p className="text-2xl capitalize">billed cast</p>
-          <CardSlider
-            credits={movie.credits && movie.credits.cast}
-            imagePath={apiConfig}
-          />
-        </div>
+    <section className="flex flex-col w-full h-full text-slate-200 text-xl font-[600]">
+      <MoviesBanner movie={movie} />
+      <div className="w-full h-full bg-gradient-to-b from-black to-[#030337]">
+        <CardSlider credits={movie.credits && movie.credits.cast} />
 
-        <div className="w-[90%] h-full mx-auto mb-[1em]">
-          <p className="text-[1.5rem]">Trailers</p>
+        <div className="w-full h-full mx-auto my-[2em] px-4 lg:px-10">
+          <p className=" mb-2">Trailers</p>
           <MovieVideos movie={movie.videos && movie.videos.results} />
         </div>
 
-        <div className="flex flex-col justify-start w-[90%] mx-auto max-[900px]:w-[100%] max-[900px]:p-4">
-          <p className="text-2xl capitalize">similar movies</p>
+        <div className="flex flex-col justify-start mx-auto w-full px-4 lg:px-10">
+          <p className="capitalize">similar movies</p>
           <MoviesSlider
             movies={movie.similar && movie.similar.results}
             imagePath={apiConfig}
           />
-          <p className="text-2xl capitalize">recommendations</p>
+          <p className="capitalize">recommendations</p>
           <MoviesSlider
             movies={movie.recommendations && movie.recommendations.results}
             imagePath={apiConfig}
--- src/pages/components/MovieDetail/CardSlider.jsx
+++ src/pages/components/MovieDetail/CardSlider.jsx
@@ -1,41 +1,33 @@
 import React from "react";
+import { apiConfig } from "../../../api";
 
-const CardSlider = ({ credits, imagePath }) => {
+const CardSlider = ({ credits }) => {
   const cardsData =
     credits &&
-    credits.map((credit) => {
-      return {
-        image: imagePath.imageUrl("original", credit.profile_path),
-        title: credit.name,
-        description: credit.character,
-      };
-    });
+    credits
+      .filter((card) => card.profile_path !== null)
+      .map((card) => {
+        return (
+          <div key={card.credit_id} className="flex flex-col items-center min-w-[200px] h-full pb-4">
+            <img
+              className="w-[200px] h-[250px] rounded-xl object-cover object-top"
+              src={apiConfig.imageUrl("original", card.profile_path)}
+              alt={card.title}
+            />
+            <p className="font-bold">{card.name}</p>
+            <p>{card.character}</p>
+          </div>
+        );
+      });
 
-  const cardContainer = (cards) => {
-    return (
-      cards &&
-      cards
-        .filter(
-          (card) => card.image !== "https://image.tmdb.org/t/p/originalnull"
-        )
-        .map((card, index) => {
-          return (
-            <div
-              key={index}
-              className="card shadow text-black bg-white rounded"
-            >
-              <img src={card.image} alt={card.title} />
-              <div className="flex flex-col items-center">
-                <p className="font-bold">{card.title}</p>
-                <p>{card.description}</p>
-              </div>
-            </div>
-          );
-        })
-    );
-  };
-
-  return <div className="cards-container">{cardContainer(cardsData)}</div>;
+  return (
+    <div className="w-full mx-auto px-4 lg:px-10">
+      <p className="text-xl font-[600] capitalize mb-2">cast</p>
+      <div className="w-full h-full flex flex-row gap-2 overflow-x-auto">
+        {cardsData}
+      </div>
+    </div>
+  );
 };
 
 export default CardSlider;
